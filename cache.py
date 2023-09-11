e488035a18e6cad185f62da0ddc15b69107bac09
Suyash Kumar <suyashkr@Suyashs-MacBook-Air.local>
2023-09-10 23:38:37 -0400

    Entire thing works

--- cache.py
+++ cache.py
@@ -3,15 +3,23 @@ from functools import lru_cache
 import yaml
 
 
+BASE_CONFIG  = "config/base.yaml"
+SECRET_CONFIG = "config/secret_template.yaml"
 @lru_cache()
-def load_config(config_path='config/base.yaml'):
+def load_config(config_path=BASE_CONFIG):
     with open(config_path, encoding="utf-8_sig") as f:
         data = yaml.load(f, Loader=yaml.FullLoader)
     return data
 
 @lru_cache()
-def load_secret(config_path='config/secret_template.yaml'):
+def load_secret(config_path=SECRET_CONFIG):
     with open(config_path, encoding="utf-8_sig") as f:
         data = yaml.load(f, Loader=yaml.FullLoader)
 
     return data
+
+def update_secret_with_calendar_id(calendar_id):
+    data = load_secret()
+    data['calendar_id'] = str(calendar_id)
+    with open(SECRET_CONFIG, 'w') as file:
+        yaml.dump(data, file)
--- service/calendar/google_calendar.py
+++ service/calendar/google_calendar.py
@@ -2,19 +2,48 @@ import datetime
 
 from gcsa.event import Event
 from gcsa.google_calendar import GoogleCalendar as _GC
+from gcsa.calendar import Calendar
 from gcsa.recurrence import Recurrence, DAILY, SU, SA
 from models.SwimmingEvent import SwimmingEventWithDay
 
 from beautiful_date import Sept, Apr
-from cache import load_secret, load_config
+from cache import load_secret, load_config, update_secret_with_calendar_id
 class GoogleCalendar:
     def __init__(self):
         self.secret = load_secret()
+        self.calendar_id = None
         self.calendar = _GC(self.secret['calendar_email'], credentials_path="credentials.json")
         self.config = load_config()
 
+        self.create_calendar_if_not_exists()
+
+    def exists_calendar(self):
+        calendar_id = self.secret['calendar_id']
+        if calendar_id == "":
+            return False
+        calendar = self.calendar.get_calendar(calendar_id)
+        if calendar is None:
+            return False
+        return True
+
+    def create_calendar(self):
+        calendar = Calendar(
+            'GT Swimming pool Calendar',
+            description='Calendar for tracking swimming pool times'
+        )
+        calendar = self.calendar.add_calendar(calendar)
+        return calendar
+
+    def update_config(self, calendar_id):
+        update_secret_with_calendar_id(calendar_id)
+
+    def create_calendar_if_not_exists(self):
+        if not self.exists_calendar():
+            calendar = self.create_calendar()
+            self.update_config(calendar.calendar_id)
+
     def add_event(self, event):
-        self.calendar.add_event(event)
+        self.calendar.add_event(event, calendar_id=self.secret['calendar_id'])
         print("Added event " + str(event))
 
     def add_event_with_details(self, swimming_event_with_day: SwimmingEventWithDay):
